3f62e925a69e4cc7328675d8136d0b9ca9d99984
SeZamora <jorgesebastianz53@gmail.com>
2025-02-08 19:07:02 -0600

    202002591: fix navbar, citas

--- Frontend/src/pages/routes/PageRoutes.jsx
+++ Frontend/src/pages/routes/PageRoutes.jsx
@@ -2,14 +2,14 @@ import { Route, Routes } from 'react-router-dom';
 import Navbar from '../../ui/components/Navbar';
 
 
-import { RecetasPage, HomePage, AppointmentsRecordPage, RegisterPacient, EditarPaciente } from '../paciente';
+import { RecetasPage, HomePage, AppointmentsRecordPage, RegisterPacient, EditarPaciente, BuscarExpediente } from '../paciente';
 
 const navigationPatient = [
   { name: 'Principal', href: '/home-page', current: false },
   { name: 'Crear Paciente', href: '/register-pacient', current: false },  
   { name: 'Generar receta', href: '/RecetasPage', current: false },
   { name: 'Ver citas', href: '/appointment-list', current: false },
-  { name: 'xxxx', href: '/profile-patient', current: false },
+  { name: 'Buscar Expediente', href: '/buscar-expediente', current: false },
 ];
 
  //<Route path="/buscar-expediente" element={<BuscarExpediente />} />
@@ -23,6 +23,7 @@ const PageRoutesComponent = () => {
         <Route path="RecetasPage" element={<RecetasPage />} />
         <Route path="register-pacient" element={<RegisterPacient />} />
         <Route path="editar-paciente/:pacienteId" element={<EditarPaciente/>} />
+        <Route path="buscar-expediente" element={<BuscarExpediente />} />
        
        
         <Route path="/" element={<HomePage />} />
